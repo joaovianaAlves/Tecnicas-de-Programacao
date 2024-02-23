2ce82b1d1c41796431ecf001134c06757963a94a
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2024-02-22 22:01:55 -0300

    Delete TecnicasDeProgramacao directory

--- TecnicasDeProgramacao/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-//        new Exercicio01().celiusToFahrenheit();
-//        new Exercicio02().MediaAritimetica();
-//        new Exercicio03().MediaGeometrica();
-          new Exercicio04().MesAnoEm();
-    }
-}
